fabf7a5491ab648f47d12dff21f1633fe704cbb7
Mayank Tripathi <mayank.tripathi786@live.com>
2021-01-04 21:48:27 +0530

    Adding more packages

--- java/codingSessions/Patterns.java
+++ java/codingSessions/Patterns.java
@@ -74,6 +74,94 @@ class Patterns {
         }
     }
     
+    /**
+     * 1    2   3   4   5
+     *      1   2   3   4
+     *          1   2   3
+     *              1   2
+     *                  1
+     */
+    void pattern05() {
+        for (int i = 5; i >= 1; i -= 1) {
+            
+            for (int k = 5; k > i; k -= 1) {
+                System.out.print("\t");
+            }
+            
+            for (int j = 1; j <= i; j += 1) {
+                System.out.print(j + "\t");
+            }
+            
+            System.out.println();
+        }
+    }
+    
+    /**
+     * 5    4   3   2   1
+     *      4   3   2   1
+     *          3   2   1
+     *              2   1
+     *                  1
+     */
+    void pattern06() {
+        for (int i = 5; i >= 1; i -= 1) {
+            
+            for (int k = 5; k > i; k -= 1) {
+                System.out.print("\t");
+            }
+            
+            for (int j = i; j >= 1; j -= 1) {
+                System.out.print(j + "\t");
+            }
+            
+            System.out.println();
+        }
+    }
+    
+    /**
+     *                  1
+     *              1   2
+     *          1   2   3
+     *      1   2   3   4
+     * 1    2   3   4   5
+     */
+    void pattern07() {
+        for (int i = 1; i <= 5; i += 1) {
+            
+            for (int k = 5; k > i; k -= 1) {
+                System.out.print("\t");
+            }
+            
+            for (int j = 1; j <= i; j += 1) {
+                System.out.print(j + "\t");
+            }
+            
+            System.out.println();
+        }
+    }
+    
+    /**
+     *                  5
+     *              5   4
+     *          5   4   3
+     *      5   4   3   2
+     * 5    4   3   2   1
+     */
+    void pattern08() {
+        for (int i = 5; i >= 1; i -= 1) {
+            
+            for (int k = 1; k < i; k += 1) {
+                System.out.print("\t");
+            }
+            
+            for (int j = 5; j >= i; j -= 1) {
+                System.out.print(j + "\t");
+            }
+            
+            System.out.println();
+        }
+    }
+    
     public static void main(String args[]) {
         
         Patterns ob = new Patterns();
@@ -81,7 +169,11 @@ class Patterns {
         // ob.pattern01();
         // ob.pattern02();
         // ob.pattern03();
-        ob.pattern04();
+        // ob.pattern04();
+        // ob.pattern05();
+        // ob.pattern06();
+        // ob.pattern07();
+        ob.pattern08();
     }
     
 }
